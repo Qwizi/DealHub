035b7e7717ba0a6d6bc7baa360cd8573d8ab050d
Mateusz Cyran <mateusz.cyran13@gamil.com>
2024-06-09 22:47:45 +0200

    stworznie kategori jak szef

--- src/dealhub/views.py
+++ src/dealhub/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from categories.models import Category
+
 
 def index(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()
+    return render(request, 'index.html', {"categories":categories} )
