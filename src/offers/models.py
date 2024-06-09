17da93a4c87a1f5d4cd0574d476ed4c19442669b
Szimura <dsw52661@dsw.edu.pl>
2024-06-09 21:58:53 +0200

    OFERTYDODAWANIEPOKAZYWANIE

--- src/offers/models.py
+++ src/offers/models.py
@@ -5,6 +5,7 @@ class Offer(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="accounts", null=True, blank=True)
     images = models.TextField()
+    description = models.TextField(default="ESZ")
     price = models.FloatField(default=50)
     is_active = models.BooleanField(default=True)
     
